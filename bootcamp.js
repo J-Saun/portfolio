47172843f07d2ae1756b114c44e2eb500311dfc0
Joshua <joshua_saun@protonmail.com>
2022-01-15 18:25:46 -0500

    just a little practice

--- bootcamp.js
+++ bootcamp.js
@@ -1,1 +1,8 @@
+// active class for nav links
+const linkList = document.querySelectorAll("nav ul li");
+// console.log(linkList);
+for (let i = 0; i < linkList.length; i++) {
+  //   console.log(linkList[i].classList);
+}
 
+//Work here
