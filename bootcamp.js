67e80e05a4e1b203127a7eec24f9e2f0ad4d4abb
Joshua <joshua_saun@protonmail.com>
2022-01-16 14:28:36 -0500

    more practice

--- bootcamp.js
+++ bootcamp.js
@@ -4,5 +4,13 @@ const linkList = document.querySelectorAll("nav ul li");
 for (let i = 0; i < linkList.length; i++) {
   //   console.log(linkList[i].classList);
 }
+// random color function
+const genRandColor = () => {
+  let r = Math.floor(Math.random() * 255);
+  let g = Math.floor(Math.random() * 255);
+  let b = Math.floor(Math.random() * 255);
+  return `rgb(${r}, ${g}, ${b})`;
+};
+// document.body.style.backgroundColor = genRandColor();
 
 //Work here
